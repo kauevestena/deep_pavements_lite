09ee76d7ddce0f7f82acd8a65318a87db1fcb317
Kauê de Moraes Vestena <kauemv2@gmail.com>
2025-09-19 07:49:58 -0300

    Add TODO for model reference in precaching_clip.py
    Added a TODO comment for the model to be used.

--- precaching/precaching_clip.py
+++ precaching/precaching_clip.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append('.')
 import clip
 
+# TODO: the model that shall be used is: https://huggingface.co/kauevestena/clip-vit-base-patch32-finetuned-surface-materials
+
 model, preprocess = clip.load("ViT-B/32",
                             #   jit=False
                               ) #Must set jit=False for training
