fd6fc204fa64f4d4bd112e6469090fcefceb9bab
kauevestena <kauemv2@gmail.com>
2026-06-15 10:46:18 -0300

    refactor: reorganize project into modular architecture and improve documentation and test coverage

--- constants.py
+++
@@ -1,9 +0,0 @@
-"""
-Backward compatibility shim.
-
-New code should import from deep_pavements.constants instead:
-
-    from deep_pavements.constants import DEVICE, default_surfaces
-"""
-
-from deep_pavements.constants import *  # noqa: F401, F403
